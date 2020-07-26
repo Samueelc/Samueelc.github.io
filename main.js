fcaf53c7e4a4908e41527b387e6c4bc66a48d457
Eric Batista <klose.eric31@gmail.com>
2020-07-26 17:25:24 -0300

    fix @media

---
+++ main.js
@@ -0,0 +1,4 @@
+gsap.to(".unico", 1.2, {delay: .3, top: "-100%", ease: Expo.easeInOut});
+
+gsap.from('.imagem', {opacity: 0, duration: 2, delay: 2, x: -60});
+gsap.from('.info', {opacity: 0, duration: 3, delay: 2.3, y: 25});
