ccc80d094576056ecf46053a02f318cf44128c70
Eric Batista <klose.eric31@gmail.com>
2020-07-26 18:06:24 -0300

    fix animation

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-gsap.to(".unico", 1.2, {delay: .3, top: "-100%", ease: Expo.easeInOut});
+gsap.to(".unico", 1.2, {delay: .8, top: "-100%", ease: Expo.easeInOut});
 
 gsap.from('.imagem', {opacity: 0, duration: 2, delay: 2, x: -60});
 gsap.from('.info', {opacity: 0, duration: 3, delay: 2.3, y: 25});
